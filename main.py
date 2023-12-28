57575d4753399d6b6dec597b1a0439cd4ea595fb
Your Name <you@example.com>
2023-12-28 21:40:45 +0300

    Görselleştirmeler eklendi

--- main.py
+++ main.py
@@ -16,19 +16,51 @@ channel_ids = [
     "UCfqDD_iRsuThW8rERHNAJNA",
 
 ]
-# Channel stats'ları çek
 channel_stats = collect_channel_stats(youtube, channel_ids)
+channel_stats['subscribers'] = pd.to_numeric(channel_stats['subscribers'], errors='coerce')
+channel_stats['views'] = pd.to_numeric(channel_stats['views'], errors='coerce')
+channel_stats['totalVideos'] = pd.to_numeric(channel_stats['totalVideos'], errors='coerce')
 
-# Görüntüleme sayıları, abone sayıları ve video sayıları üzerine genel analiz
+
+# Genel analiz için görüntüleme sayıları, abone sayıları ve video sayıları
 print("Toplam Görüntülenme Sayısı:", channel_stats['views'].sum())
 print("Toplam Abone Sayısı:", channel_stats['subscribers'].sum())
 print("Toplam Video Sayısı:", channel_stats['totalVideos'].sum())
 
 # Görselleştirme için seaborn kullanarak bar plot
-plt.figure(figsize=(12, 6))
-sns.barplot(x='channelName', y='views', data=channel_stats)
-plt.title('Kanal Görüntülenme Sayıları')
+plt.figure(figsize=(16, 10))
+
+# Subplot 1: Toplam Görüntülenme Sayıları
+plt.subplot(2, 2, 1)
+sns.barplot(x='channelName', y='views', data=channel_stats, color='purple')
+plt.title('Toplam Görüntülenme Sayıları')
 plt.xticks(rotation=45, ha='right')
+
+# Subplot 2: Toplam Abone Sayıları
+plt.subplot(2, 2, 2)
+sns.barplot(x='channelName', y='subscribers', data=channel_stats)
+plt.title('Toplam Abone Sayıları')
+plt.xticks(rotation=45, ha='right')
+
+
+# Subplot 3: Toplam Video Sayıları
+plt.subplot(2, 2, 3)
+sns.barplot(x='channelName', y='totalVideos', data=channel_stats, color= 'orange')
+plt.title('Toplam Video Sayıları')
+plt.xticks(rotation=45, ha='right')
+
+# Subplot 4: Video Detayları (örneğin: görüntülenmeler, beğeniler)
+video_ids = collect_video_ids(youtube, channel_stats['playlistId'].iloc[0])
+video_details = collect_video_details(youtube, video_ids)
+video_details_df = pd.DataFrame(video_details)
+plt.subplot(2, 2, 4)
+sns.scatterplot(x='viewCount', y='likeCount', data=video_details_df, color='salmon')
+plt.title('Video Görüntülenme ve Beğeni Sayıları')
+
+# Grafikleri göster
+plt.tight_layout()
 plt.show()
+
+
 
 
